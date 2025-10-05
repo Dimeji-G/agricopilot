169ba595e18597205757f7be75598b9764044d73
dimeji <ukwedjedimeji@gmail.com>
2025-10-05 06:47:01 +0100

    AgriCopilot Web Push: Haew^2

--- Agricopilot/settings.py
+++ Agricopilot/settings.py
@@ -15,11 +15,11 @@ SECRET_KEY =  config('SECRET_KEY', default='django-insecure-ohap$t9bfp^b9sz^10wy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,agricopilot.azurewebsites.net', cast=lambda v: [s.strip() for s in v.split(',')])
 
 CSRF_TRUSTED_ORIGINS = config(
     'CSRF_TRUSTED_ORIGINS',
-    default='',
+    default='https://agricopilot.azurewebsites.net',
     cast=lambda v: [s.strip() for s in v.split(',') if s.strip()]
 )
 
